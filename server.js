79049f30e153fff2b7c19181fdb4c1c104a9a186
Glitch (codersx-express-lesson1) <none>
2020-06-29 09:27:52 +0000

    💺🍲 Checkpoint ./views/index.pug:18003481/559 ./server.js:18003481/4768 ./package.json:18003481/1394

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 // https://expressjs.com/en/starter/basic-routing.html
 
 var todos = [
@@ -31,13 +34,20 @@ app.get("/todos", function(req, res) {
     return active.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
   res.render("index", {
-    title: "3-Query Parameters",
+    title: "4-post method",
     message: "TODO - LIST",
     search: q,
     todoList: matchedActive
   });
 });
+app.get('/todos/create', function(req, res) {
+	res.render('create');
+});
 
+app.post('/todos/create', function(req, res){
+	todos.push(req.body.todo);
+	res.redirect('/');
+});
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
